944c1c10be0d23401701d6aaf9cfbe47e7abcf3a
Masahiko Funaki <mfunaki@gmail.com>
2025-08-26 10:54:01 +0900

    Initial Commit

---
+++ iOSClockTZ/iOSClockTZApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSClockTZApp.swift
+//  iOSClockTZ
+//
+//  Created by Masahiko Funaki on 2025/08/26.
+//
+
+import SwiftUI
+
+@main
+struct iOSClockTZApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
